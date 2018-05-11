8ee112b5b2d0680f68eba13953228b7ac85e404d
Anton Parkhomenko <mailbox@chuwy.me>
2018-05-12 00:20:45 +0200

    Add parser

--- app/Main.hs
+++ app/Main.hs
@@ -2,24 +2,34 @@ module Main where
 
 import Lib
 
+import Data.List
 import Control.Monad.Loops (whileM_)
+
+data Command = Get String | Put String String | Exit
 
 main :: IO ()
 main = whileM_ interpreter $ pure ()
 
-execute :: String -> IO ()
-execute "put" = putStrLn "putting value"
-execute "get" = putStrLn "getting value"
-execute other = putStrLn $ "You just entered " ++ other
+execute :: Command -> IO ()
+execute (Get key) = putStrLn $ "getting key " ++ key
+execute (Put key value) = putStrLn $ "putting key " ++ key
+
+parseCommand :: String -> Either String Command
+parseCommand command = case words command of ["get", key] -> Right $ Get key
+                                             ["put", key, value] -> Right $ Put key value
+                                             ["exit"] -> Right Exit
+                                             other -> Left $ intercalate " " other
 
 interpreter :: IO Bool
 interpreter = do
     line <- getLine
-    if line == "exit"
-        then do
-            putStrLn "bye"
-            pure False
-        else do
-            execute line
-            pure True
+    case parseCommand line of Right Exit -> do
+                                putStrLn "bye"
+                                pure False
+                              Right command -> do
+                                execute command
+                                pure True
+                              Left unknown -> do
+                                putStrLn $ "Unknown command " ++ unknown ++ ". Aborting!"
+                                pure False
 
