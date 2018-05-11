a8b31872735bb3a5d860f20a248df300a2917725
Anton Parkhomenko <mailbox@chuwy.me>
2018-05-12 01:08:11 +0200

    Implement writing to file

--- app/Main.hs
+++ app/Main.hs
@@ -7,12 +7,14 @@ import Control.Monad.Loops (whileM_)
 
 data Command = Get String | Put String String | Exit
 
+database = "database.txt"
+
 main :: IO ()
 main = whileM_ interpreter $ pure ()
 
 execute :: Command -> IO ()
-execute (Get key) = putStrLn $ "getting key " ++ key
-execute (Put key value) = putStrLn $ "putting key " ++ key
+execute (Get key) = get key
+execute (Put key value) = put key value
 
 parseCommand :: String -> Either String Command
 parseCommand command = case words command of ["get", key] -> Right $ Get key
@@ -33,3 +35,15 @@ interpreter = do
                                 putStrLn $ "Unknown command " ++ unknown ++ ". Aborting!"
                                 pure False
 
+put :: String -> String -> IO ()
+put key value = appendFile database (key ++ "\t" ++ value ++ "\n")
+
+get :: String -> IO ()
+get key = do
+    content <- readFile database
+    let linesOfFiles = fmap words $ lines content
+    print $ find (searchByKey key) linesOfFiles
+
+searchByKey :: String -> [String] -> Bool
+searchByKey key kvs = case kvs of h : _ -> h == key
+                                  _ -> False
