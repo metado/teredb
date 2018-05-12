24e1734d4728529c5200fafbca2546c257667068
Anton Parkhomenko <mailbox@chuwy.me>
2018-05-12 12:53:27 +0200

    Add CLI args parsing

--- app/Main.hs
+++ app/Main.hs
@@ -2,21 +2,33 @@ module Main where
 
 import Lib
 
+import System.Environment
 import System.Exit
 import Data.List
 
 data Command = Get String | Put String String | Exit
 
-database :: String
-database = "database.txt"
+data Config = Config String
 
 main :: IO ()
-main = sequence_ $ repeat interpreter
+main = do
+    args <- getArgs
+    run args
+    
+run :: [String] -> IO ()
+run args = do
+    case parseArgs args of Right config -> sequence_ $ repeat (interpreter config)
+                           Left error -> die error
+    
+execute :: Config -> Command -> IO ()
+execute config (Get key) = get config key
+execute config (Put key value) = put config key value
+execute config Exit = putStrLn "Bye!" >> exitWith ExitSuccess
 
-execute :: Command -> IO ()
-execute (Get key) = get key
-execute (Put key value) = put key value
-execute Exit = putStrLn "Bye!" >> exitWith ExitSuccess
+parseArgs :: [String] -> Either String Config
+parseArgs [] = Left "Expected one argument, none given"
+parseArgs (p : []) = Right $ Config p
+parseArgs other = Left $ "Unexpected arguments " ++ (intercalate " " other)
 
 parseCommand :: String -> Either String Command
 parseCommand command = case words command of ["get", key] -> Right $ Get key
@@ -24,19 +36,19 @@ parseCommand command = case words command of ["get", key] -> Right $ Get key
                                              ["exit"] -> Right Exit
                                              other -> Left $ intercalate " " other
 
-interpreter :: IO ()
-interpreter = do
+interpreter :: Config -> IO ()
+interpreter config = do
     line <- getLine
     case parseCommand line of Right command -> do
-                                execute command
+                                execute config command
                               Left unknown -> do
                                 putStrLn $ "Unknown command " ++ unknown
 
-put :: String -> String -> IO ()
-put key value = appendFile database (key ++ "\t" ++ value ++ "\n")
+put :: Config -> String -> String -> IO ()
+put (Config database) key value = appendFile database (key ++ "\t" ++ value ++ "\n")
 
-get :: String -> IO ()
-get key = do
+get :: Config -> String -> IO ()
+get (Config database) key = do
     content <- readFile database
     let linesOfFiles = fmap words $ lines content
     print $ find (searchByKey key) linesOfFiles
